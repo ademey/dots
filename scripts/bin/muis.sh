41c7bc4bd055eedb6f8c02129c4086d65f0b018b
Anne DeMey <anneyvdemey@gmail.com>
2024-12-17 14:56:07 -0600

    Modifying muis to save data

--- scripts/bin/muis.sh
+++ scripts/bin/muis.sh
@@ -1,21 +1,41 @@
 #!/usr/bin/env bash
 
-# TODO: Fallback for no results
+muis_data_home=${XDG_DATA_HOME:-~/.local/share}/muis
 dict="muiswerk"
 dict_url="https://www.ensie.nl"
+target_file="${muis_data_home}/${1}.html"
+page_data=""
+
 if [ $1 ]; then
-  data=$(curl -s $dict_url/$dict/$1)
-  # Get the main content area and replace any relative urls with the full domain
-  default=$(echo $data | pup '.content' | sed -e 's|href="/|href="http://www.ensie.nl/|')
-  found=$(echo $default | pup '[itemprop="articleSection"]')
-  if [ -n "${found}" ]; then
-    # search=$(echo $data | pup '[itemprop="articleSection"]' | sed -e 's|href="/|href="http://www.ensie.nl/|')
-    echo $found | lynx -dump -stdin
-  else
-    echo $default | lynx -dump -stdin
-  fi
+    echo "🐭 Zoeken... ${1}"
 else
-  echo "Een zoekwoord aub"
+    echo "🐭 Zoekwoord aub..."
+    exit 0
+fi
+
+if [ ! -d $muis_data_home ]; then
+    mkdir $muis_data_home
+fi
+
+if [ -f $target_file ]; then
+    echo "🧀 Laden vanuit cache"
+    page_data=$(cat $target_file)
+else
+    echo "⌛ Bezig met downloaden"
+    page_data=$(curl -s $dict_url/$dict/$1)
+fi
+
+
+# Get the main content area and replace any relative urls with the full domain
+default=$(echo $page_data | pup '.content' | sed -e 's|href="/|href="http://www.ensie.nl/|')
+found=$(echo $default | pup '[itemprop="articleSection"]')
+if [ -n "${found}" ]; then
+    # Write the data if we found a definition
+    echo ""
+    echo $page_data > $target_file
+    echo $found | lynx -dump -stdin
+else
+    echo $default | lynx -dump -stdin
 fi
 
 
