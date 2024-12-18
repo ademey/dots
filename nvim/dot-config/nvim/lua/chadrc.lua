f9fbbadfcf107ff1fb7151907943b2026b9fe42b
Anne DeMey <anneyvdemey@gmail.com>
2024-12-18 09:53:04 -0600

    Customizing nvim dashboard

--- nvim/dot-config/nvim/lua/chadrc.lua
+++ nvim/dot-config/nvim/lua/chadrc.lua
@@ -8,15 +8,51 @@ local M = {}
 M.base46 = {
   theme = "chadracula",
 
-  -- hl_override = {
-  -- 	Comment = { italic = true },
-  -- 	["@comment"] = { italic = true },
-  -- },
+  hl_override = {
+    Comment = { italic = true },
+    ["@comment"] = { italic = true },
+  },
 }
 
 M.nvdash = {
   load_on_startup = true,
+  header = {
+    "      .-'~~~-.           ",
+    "    .'o  oOOOo`.         ",
+    "   :~~~-.oOo   o`.       ",
+    "    `. @ ~-.  oOOo.      ",
+    "      `.; / ~.  OO:      ",
+    "      .'  ;-- `.o.'      ",
+    "     ,'  ; ~~--'~        ",
+    "     ;  ;                ",
+    "___\\;_\\//___[|]________",
+    "                         ",
+  },
+
+  buttons = {
+    { txt = "  Find File", keys = "ff", cmd = "Telescope find_files" },
+    { txt = "  Recent Files", keys = "fo", cmd = "Telescope oldfiles" },
+    { txt = "󰈭  Find Word", keys = "fw", cmd = "Telescope live_grep" },
+    { txt = "󱥚  Themes", keys = "th", cmd = ":lua require('nvchad.themes').open()" },
+    { txt = "  Mappings", keys = "ch", cmd = "NvCheatsheet" },
+
+    { txt = "─", hl = "NvDashFooter", no_gap = true, rep = true },
+
+    {
+      txt = function()
+        local stats = require("lazy").stats()
+        local ms = math.floor(stats.startuptime) .. " ms"
+        -- return "  Loaded " .. stats.loaded .. "/" .. stats.count .. " plugins in " .. ms
+        return "Mag ik je virtueel beffen?"
+      end,
+      hl = "NvDashFooter",
+      no_gap = true,
+    },
+
+    { txt = "─", hl = "NvDashFooter", no_gap = true, rep = true },
+  },
 }
+
 -- https://www.reddit.com/r/neovim/comments/1fqole3/comment/lpvenci/?utm_source=share&utm_medium=web3x&utm_name=web3xcss&utm_term=1&utm_content=share_button
 -- TODO: Test deleting this config
 M.mason = {
