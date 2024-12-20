8442d1e7dab38b8d8495607ec8b6b9ee41263eff
Anne DeMey <anneyvdemey@gmail.com>
2024-12-20 00:56:59 -0600

    Swwwitching to Swww backgrounds

--- scripts/bin/post-theme.sh
+++ scripts/bin/post-theme.sh
@@ -5,17 +5,19 @@ IMG="$(< "${HOME}/.cache/wal/wal")"
 # Generated through pywal templates
 ln -s "${HOME}/.cache/wal/hyprlock.conf" "${HOME}/.config/hypr/hyprlock.conf"
 ln -s "${HOME}/.cache/wal/mako" "${HOME}/.config/mako/config"
-# Maybe link later, right now it is fine with whatever as default
+# Not using this right now
 # ln -s "${HOME}/.cache/wal/hyprpaper.conf" "${HOME}/.config/hypr/hyprpaper.conf"
 
-hyprctl hyprpaper preload $IMG
-hyprctl hyprpaper wallpaper ", $IMG"
+# hyprctl hyprpaper preload $IMG
+# hyprctl hyprpaper wallpaper ", $IMG"
 
+swww img --transition-type random $IMG
 pkill waybar && hyprctl dispatch exec waybar
 makoctl reload
 
 # Would be cool to do other effects
-magick $IMG -blur 0x10 "${HOME}/.cache/hyprlock/background.png"
-bash "${HOME}/.config/wal/preview.sh"
+magick $IMG -blur 0x15 "${HOME}/.cache/hyprlock/background.png"
 
-## hyprctl keyword general:col.active_border "rgba(FFF000aa)"
+# Generates an image with the background and colors
+# bash "${HOME}/.config/wal/preview.sh"
+
--- scripts/bin/theme.sh
+++ scripts/bin/theme.sh
@@ -2,8 +2,18 @@
 
 # https://github.com/dylanaraps/pywal/wiki/Getting-Started#using-a-custom-wallpaper-setter
 
-wallpapers="${HOME}/Wallpaper"
-op=$(find $wallpapers -type f | sed -e "s|$wallpapers||" | tofi --prompt-text=" wal: " --config="${HOME}/.config/tofi/fullscreen")
+# TODO: Check that this is an image in wallpaper because of yazi script use
+image=$1
+op=0
+if [ $1 ]; then
+    echo "Using file"
+    # notify-send "theme" $1
+    op=$1
+else
+    wallpapers="${HOME}/Wallpaper"
+    op=$(find $wallpapers -type f | sed -e "s|$wallpapers||" | tofi --prompt-text=" wal: " --config="${HOME}/.config/tofi/fullscreen")
+    image="$wallpapers/$op"
+fi
 # Make an easier way to change these
 # op2=$( echo -e "lighten\ndarken" | tofi --prompt-text=" temp: " )
 # op3=$( echo -e "1\n2\n3\n4\n5\n6\n7\n8\n9" | tofi --prompt-text=" saturate: " )
@@ -14,11 +24,11 @@ op=$(find $wallpapers -type f | sed -e "s|$wallpapers||" | tofi --prompt-text=" 
 # -e skip reload bar
 # -s skip terminals
 if [ $op ]; then
-  wal -n -t -e -s\
-    --saturate 0.5 \
-    --cols16 "darken" \
-    -i "$wallpapers/$op" -o "${HOME}/bin/post-theme.sh"
+    wal -n -t -e -s\
+        --saturate 0.5 \
+        --cols16 "darken" \
+        -i $image -o "${HOME}/bin/post-theme.sh"
 
 else
     echo "canceled"
-fi   
+fi
