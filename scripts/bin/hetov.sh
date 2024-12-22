96027d8954a51b1fc1429608ffc10f69cf8dea53
Anne DeMey <anneyvdemey@gmail.com>
2024-12-22 11:18:58 -0600

    Progress on ov

--- scripts/bin/hetov.sh
+++ scripts/bin/hetov.sh
@@ -2,24 +2,27 @@
 
 # https://www.transitchicago.com/developers/ttdocs/#_Toc296199905
 #
-# export CTA_KEY=$(pass api/cta | head -n 1)
-tmp_dir=$HOME/.cache/hetov
-settings="$tmp_dir/settings"
-stops_data="$tmp_dir/stops.json"
-arrivals="$tmp_dir/arrivals.json"
+# pass -c api/cta && export CTA_KEY=$(wl-paste)
+hetov_data_home=${XDG_DATA_HOME:-~/.local/share}/hetov
+
+# hetov_data_home=$HOME/.cache/hetov
+# settings="$hetov_data_home/settings"
+stops_data="$hetov_data_home/stops.json"
+arrivals="$hetov_data_home/arrivals.json"
+arrivals_dir="$hetov_data_home/arrivals"
 
 api_url=http://lapi.transitchicago.com/api/1.0
 
-refresh=0
+refresh=1
 
 # Stations have multiple stops
-# station="40570" # California
-# stop_id="30112"
+# stop_id="30112" # California
 stop_id=""
 map_id=""
 
 # echo "$(cat ~/Notes/ascii/cta.ascii)"
-echo "🚋🚇 Het OV"
+echo "🚋 Het OV"
+echo "---------"
 
 if [ -z "$CTA_KEY" ]; then
     echo "🗝 API key missing"
@@ -29,30 +32,37 @@ fi
 while getopts 'rls:' opt; do
     case "$opt" in
         s)
-            stop_id=$OPTARG
+            map_id=$OPTARG
             ;;
         r)
-            refresh=1
+            refresh=0
             ;;
         l)
     esac
 done
 
+if [ ! -d $hetov_data_home ]; then
+    echo "Setting up"
+    mkdir -p $hetov_data_home
+fi
+
 if [ ! -f $stops_data ]; then
-    echo '📦 Loding "L" database'
     # https://data.cityofchicago.org/Transportation/CTA-System-Information-List-of-L-Stops/8pix-ypme/about_data
-    curl -s "https://data.cityofchicago.org/resource/8pix-ypme.json" > $stops_data
+    gum spin --spinner dot --title "📦 Loding "L" database" -- sleep 1 && curl -s "https://data.cityofchicago.org/resource/8pix-ypme.json" > $stops_data
+
+
 fi
 
-if [ ! -n "$stop_id" ]; then
-    # echo "Select a Line"
-    line_name=$(gum choose --header "Line:" --height 15 "red" "blue" "g" "brn" "p" "pexp" "y" "pnk" "o")
 
-    # echo "Select a Staion"
-    stop_name=$(jq -r ".[] | select(.\"$line_name\" == true) | .stop_name" $stops_data | sort | gum choose --header "Station:" --height 15)
-    stop_id=$(jq -r ".[] | select(.stop_name == \"$stop_name\") | .stop_id" $stops_data)
-    echo "> $stop_name > $stop_id"
-fi
+function setup_lines() {
+    real_names=("Blue" "Green" "Brown" "Purple" "Purple Exp" "Yellow" "Pink" "Orange")
+    codes=("blue" "g" "brn" "p" "pexp" "y" "pnk" "o")
+
+    echo "Red,red" > "$hetov_data_home/lines.csv"
+    for i in ${!real_names[@]}; do
+        echo "${real_names[$i]},${codes[$i]}" >> "$hetov_data_home/lines.csv"
+    done
+}
 
 
 function relative() {
@@ -88,21 +98,52 @@ function flagged() {
     fi
 }
 
-selected_stop=$(jq -r ".[] | select(.stop_id == \"$stop_id\")" $stops_data)
+function pick_stop() {
+    # echo "Select a Line"
+    real_names=("Red", "Blue", "Green", "Brown", "Purple", "Purple Exp", "Yellow", "Pink", "Orange")
+    codes=("red", "blue", "g", "brn", "p", "pexp", "y", "pnk", "o")
+    # awk -F ',' '{printf "%-10s %-4s\n", $1, $2}'
+    # line_name=$(cat "$hetov_data_home/lines.csv" | gum choose --header "Line:" --height 10)
+    # line_code=$(echo $line_name | awk -F ',' '{print $NF}')
 
-if [ ! -n "$selected_stop" ]; then
-    echo "No stop data for stop id: $stop_id"
+    # echo "Select a Staion"
+    # stop_name=$(jq -r ".[] | select(.\"$line_code\" == true) | .stop_name" $stops_data | sort | gum choose --header "Station:" --height 10)
+    # stop_name=$(jq -r ".[] | select(.\"$line_code\" == true) | [.map_id, .station_name] | @csv" $stops_data | sort | uniq | gum choose --header "Station:" --height 10)
+    stop_name=$(jq -r ".[] | [.map_id, .station_descriptive_name] | @csv" $stops_data | sort | uniq | awk -F "," '{print $2 " " $1}' | sort | sed 's|"||g' | gum filter --header "Station:" --height 10)
+    selected_map_id=$(echo $stop_name | awk -F ' ' '{print $NF}')
+    # echo $(jq -r ".[] | select(.map_id == \"$selected_map_id\") | .map_id" $stops_data)
+    # echo "> $stop_name > $stop_id"
+    echo $selected_map_id
+}
+
+
+
+
+if [ ! -n "$map_id" ]; then
+    setup_lines
+    # stop_id=$(pick_stop)
+    map_id=$(pick_stop)
+fi
+
+
+
+if [ ! -n "$map_id" ]; then
+    echo "No stop data for stop id: $map_id"
     exit 1
 fi
 
+
+selected_stop=$(jq -r ".[] | select(.map_id == \"$map_id\")" $stops_data)
 station_name=$(val "$selected_stop" '.station_descriptive_name')
-map_id=$(val "$selected_stop" '.map_id')
+# map_id=$(val "$selected_stop" '.map_id')
 
 
 if [ $refresh -eq 1 ]; then
     req="$api_url/ttarrivals.aspx?mapid=$map_id&max=10&outputType=JSON&key=$CTA_KEY"
     # echo "Fetching data: $req"
-    curl -s $req > $arrivals
+    gum spin --spinner dot -- sleep 1 && curl -s $req > $arrivals
+else
+    echo "Using cache"
 fi
 
 refresh_time=$(jq -r '.ctatt.tmst' $arrivals)
@@ -113,9 +154,6 @@ since_refresh=$(relative $refresh_time)
 echo ""
 printf "%-25s -- %15s\n" "$station_name" "$since_refresh"
 echo "------------------------------------------"
-
-
-
 
 cat "$arrivals" | jq -c '.ctatt | .eta[]' | while IFS= read -r item; do
     # Extract individual properties from each item
