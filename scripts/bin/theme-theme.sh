8c77f37b4c104f7443a03b4b9df79e980d1a3700
Anne DeMey <anneyvdemey@gmail.com>
2025-01-08 22:18:46 -0600

    Keeping track of selected theme name

--- scripts/bin/theme-theme.sh
+++ scripts/bin/theme-theme.sh
@@ -8,11 +8,13 @@
 #
 
 
-op=$( wal --theme | grep "^ - " | awk '{print $2}' | tofi --prompt-text=" colors: " --config="${HOME}/.config/tofi/fullscreen")
+c_config=$( cat "$HOME/.cache/waybar/wal-theme-name" )
+op=$( wal --theme | grep "^ - " | awk '{print $2}' | tofi --prompt-text=" theme ($c_config): " --config="${HOME}/.config/tofi/fullscreen")
 
 
 if [ $op ]; then
     wal --theme $op -o ~/bin/theme-post.sh
+    echo $op > "$HOME/.cache/waybar/wal-theme-name"
     # Write the selections to cache file
     # pkill waybar & hyprctl dispatch exec waybar
 else
