4d6295894e97f471ef36aebf7af010af65b10372
Anne DeMey <anneyvdemey@gmail.com>
2025-01-13 13:13:00 -0600

    Lots
    Starttree links
    hypr keybinds
    quicklinks qutebrowser
    git icons starship

--- qutebrowser/dot-config/qutebrowser/config.py
+++ qutebrowser/dot-config/qutebrowser/config.py
@@ -1,2 +1,9 @@
 config.load_autoconfig()
+
+config.bind('M', 'hint links spawn mpv {hint-url}')
+config.bind('Z', 'hint links spawn kitty -e yt-dlp {hint-url}')
+config.bind('t', 'set-cmd-text -s :open -t')
+
+
+# Bottom!
 config.source('qutewal.py')
