5cbfe8e96958e6517b2bf447087b93ccac4b48d7
Anne DeMey <anneyvdemey@gmail.com>
2025-01-16 11:26:59 -0600

    Adding yazi theme set integration

--- scripts/bin/theme-img.sh
+++ scripts/bin/theme-img.sh
@@ -2,7 +2,7 @@
 
 if [ -f $1 ]; then
   echo $1 > ${HOME}/.cache/wal/wal
-  swww img --transition-type random $1
+  swww img --transition-type grow $1
   magick $1 -blur 0x15 "${HOME}/.cache/hyprlock/background.png"
 fi
 
