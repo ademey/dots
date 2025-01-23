83a84d1a4df095ddac3c0020d4bcf85fe664ecb8
Anne DeMey <anneyvdemey@gmail.com>
2025-01-22 18:03:50 -0600

    Nvim from scratch

---
+++ nvim-lazy/dot-config/nvim/lua/user/plugins/alpha.lua
@@ -0,0 +1,52 @@
+
+-- see if the file exists
+local function file_exists(file)
+  local f = io.open(file, "rb")
+  if f then
+    f:close()
+  end
+  return f ~= nil
+end
+
+-- get all lines from a file, returns an empty
+-- list/table if the file does not exist
+local function lines_from(file)
+  if not file_exists(file) then
+    return {}
+  end
+  local lines = {}
+  for line in io.lines(file) do
+    lines[#lines + 1] = line
+  end
+  return lines
+end
+
+return {
+  "goolord/alpha-nvim",
+  event = "VimEnter",
+  config = function()
+    local alpha = require("alpha")
+    local dashboard = require("alpha.themes.dashboard")
+
+    
+    -- Set header
+    local pic = lines_from "/home/anne/Notes/ascii/mushroom-2.ascii"
+    dashboard.section.header.val = pic
+
+    -- Set menu
+    dashboard.section.buttons.val = {
+      dashboard.button("e", "  > New File", "<cmd>ene<CR>"),
+      dashboard.button("SPC ee", "  > Toggle file explorer", "<cmd>NvimTreeToggle<CR>"),
+      dashboard.button("SPC ff", "󰱼 > Find File", "<cmd>Telescope find_files<CR>"),
+      dashboard.button("SPC fs", "  > Find Word", "<cmd>Telescope live_grep<CR>"),
+      dashboard.button("SPC wr", "󰁯  > Restore Session For Current Directory", "<cmd>SessionRestore<CR>"),
+      dashboard.button("q", " > Quit NVIM", "<cmd>qa<CR>"),
+    }
+
+    -- Send config to alpha
+    alpha.setup(dashboard.opts)
+
+    -- Disable folding on alpha buffer
+    vim.cmd([[autocmd FileType alpha setlocal nofoldenable]])
+  end,
+}
---
+++ nvim-lazy/dot-config/nvim/lua/user/plugins/vim-maximizer.lua
@@ -0,0 +1,7 @@
+return {
+  "szw/vim-maximizer",
+  -- load on keys
+  keys = {
+    { "<leader>sm", "<cmd>MaximizerToggle<CR>", desc = "Maximize/minimize a split" },
+  },
+}
